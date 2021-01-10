e91597629d9f97fbac81dcc40a1bf7db43c5ee3a
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-10 16:56:29 +0900

    [Add] Chapter05.java & Chapter05.class

---
+++ JavaWcp1/src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 9250616532L;
+		double double1 = 12.25;
+		boolean boolean1 = true;
+
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+
+		Float float1 = 0.1f;
+		Integer Integer1 = 25;
+		Character Character1 = 'a';
+
+		System.out.println(float1);
+		System.out.println(Integer1);
+		System.out.println(Character1);
+		
+		int[] array = new int[] {10, 20, 30, 40, 50};
+		System.out.println(array[3]);
+	}
+}
