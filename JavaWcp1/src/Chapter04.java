734eb96b9d6da5e027d15e39a5d2142dc7c7dd88
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-10 11:37:34 +0900

    [Add] Chapter04.java & Chapter04.class

---
+++ JavaWcp1/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "山田太郎";
+		System.out.println(name);
+
+		name = "こんにちは！";
+		System.out.println(name);
+
+		int number = 7;
+		System.out.println(number);
+	}
+}
