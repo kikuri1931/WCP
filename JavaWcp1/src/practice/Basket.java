fe9be72addcdc8fba53815d48710d55e4952d673
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-20 12:38:07 +0900

    [Add] Basket.java & Basket.class

---
+++ JavaWcp1/src/practice/Basket.java
@@ -0,0 +1,29 @@
+package practice;
+
+import java.math.BigDecimal;
+
+public class Basket<E extends Fruit> {
+	private E elem;
+	private final BigDecimal PRICE = new BigDecimal(100);
+
+	Basket(E e) {
+		elem = e;
+	}
+
+	void replace(E e) {
+		elem = e;
+	}
+
+	E get() {
+		return elem;
+	}
+
+	void printName() {
+		System.out.println("かごの中身は" + elem.getName() + "です。");
+	}
+
+	void printTotalPrice() {
+		BigDecimal totalPrice = PRICE.add(elem.getPrice());
+		System.out.println(elem.getName() + "の入ったかごの合計は" + totalPrice + "円です。");
+	}
+}
