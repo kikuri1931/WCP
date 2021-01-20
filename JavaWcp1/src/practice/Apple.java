5105cb94c5ca20749d8427d8b58e8dc3505542a4
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-20 12:36:29 +0900

    [Add] Apple.java & Apple.class

---
+++ JavaWcp1/src/practice/Apple.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.math.BigDecimal;
+
+class Apple extends Fruit {
+	Apple(String name, BigDecimal price) {
+		super(name, price);
+	}
+
+	@Override
+	protected String getName() {
+		return name;
+	}
+
+	@Override
+	protected BigDecimal getPrice() {
+		return price;
+	}
+}
