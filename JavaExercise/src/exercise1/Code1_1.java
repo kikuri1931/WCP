b7dbcb621a32f2cbcbcecc9b88caac875b37fcf9
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-24 12:01:54 +0900

    [Add] Code1-1.java & code1-1.class

---
+++ JavaExercise/src/exercise1/Code1_1.java
@@ -0,0 +1,10 @@
+package exercise1;
+
+public class Code1_1 {
+	public static void main(String[] args) {
+		System.out.println("RPG:スッキリ魔王討伐");
+		System.out.println("Var.0.1 by 湊");
+		System.out.println("<ただいま鋭意学習中・制作中>");
+		System.out.println("プログラムを終了します。");
+	}
+}
