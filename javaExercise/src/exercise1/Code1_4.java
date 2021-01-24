6a6fa74fb49f9411d48b54ef315e122a19624641
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-24 13:30:41 +0900

    [Add] Code1_4.java

---
+++ javaExercise/src/exercise1/Code1_4.java
@@ -0,0 +1,16 @@
+package exercise1;
+
+public class Code1_4 {
+	public static void main(String[] args) {
+
+		final double PI = 3.14;
+		int pie = 5;
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * PI);
+
+		System.out.println("パイの半径を倍にします");
+		pie = 10;
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * PI);
+	}
+}
