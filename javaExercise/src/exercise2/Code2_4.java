d93b98e51d58cf7206996ade6b185a4fd3951b61
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-26 20:04:44 +0900

    [Add] Code2_4.java

---
+++ javaExercise/src/exercise2/Code2_4.java
@@ -0,0 +1,10 @@
+package exercise2;
+
+public class Code2_4 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		System.out.println(++a + 50);
+		System.out.println(b++ + 50);
+	}
+}
