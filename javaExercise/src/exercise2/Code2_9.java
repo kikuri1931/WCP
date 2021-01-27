334331afd1da9134234d25f51adbd3603c563293
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-27 18:30:30 +0900

    [Add] Code2_9.java

---
+++ javaExercise/src/exercise2/Code2_9.java
@@ -0,0 +1,10 @@
+package exercise2;
+
+public class Code2_9 {
+	public static void main(String[] args) {
+		String name = "すがわら";
+		String message;
+		message =name + "さん、こんにちは";
+		System.out.println(message);
+	}
+}
