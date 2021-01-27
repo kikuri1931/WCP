80d6f0761b316278335b2d6b6de1d0e9092d7a46
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-27 19:08:01 +0900

    [Add] Code2_11.java

---
+++ javaExercise/src/exercise2/Code2_11.java
@@ -0,0 +1,11 @@
+package exercise2;
+
+public class Code2_11 {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		int m = Math.max(a, b);
+
+		System.out.println("比較実験：" + a + "と" + b + "とで大きい方は･･･" + m);
+	}
+}
