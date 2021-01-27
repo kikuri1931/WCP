64f072f2d9ac8440658d8de0b31a54c2d55e0aa1
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-27 19:01:03 +0900

    [Add] Code2_10.java

---
+++ javaExercise/src/exercise2/Code2_10.java
@@ -0,0 +1,10 @@
+package exercise2;
+
+public class Code2_10 {
+	public static void main(String[] args) {
+		String name = "すがわら";
+		System.out.print("私の名前は");
+		System.out.print(name);
+		System.out.print("です");
+	}
+}
