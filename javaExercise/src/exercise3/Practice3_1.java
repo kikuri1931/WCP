f548737ba0c198a9e77940077928fb2b98f98c51
ys194 <ys194@LAPTOP-M89T4L4C.mshome.net>
2021-02-01 20:06:53 +0900

    [Add] Practice3_1.java

---
+++ javaExercise/src/exercise3/Practice3_1.java
@@ -0,0 +1,18 @@
+package exercise3;
+
+public class Practice3_1 {
+	public static void main(String[] args) {
+		int isHungry = 0;
+		String food = "りんご";
+
+		System.out.println("こんにちは");
+
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("はらぺこです");
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");
+	}
+}
