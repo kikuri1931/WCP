0c2620c7175ce8a5b104d88f60b3e7347871710f
ys194 <ys194@LAPTOP-M89T4L4C.mshome.net>
2021-02-01 19:15:06 +0900

    [Add] Code3_7.java

---
+++ javaExercise/src/exercise3/Code3_7.java
@@ -0,0 +1,13 @@
+package exercise3;
+
+public class Code3_7 {
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.print(i * j);
+				System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+}
