01f640fe05aff81850e3803e3442bfefb5eb1b5c
ys194 <ys194@LAPTOP-M89T4L4C.mshome.net>
2021-02-01 20:28:06 +0900

    [Add] Practice3_3.java

---
+++ javaExercise/src/exercise3/Practice3_3.java
@@ -0,0 +1,23 @@
+package exercise3;
+
+public class Practice3_3 {
+	public static void main(String[] args) {
+		System.out.print("[メニュー]1:検索2:登録3:削除4:変更>");
+		int selected = new java.util.Scanner(System.in).nextInt();
+
+		switch (selected) {
+		case 1:
+			System.out.println("検索します");
+			break;
+		case 2:
+			System.out.println("登録します");
+			break;
+		case 3:
+			System.out.println("削除します");
+			break;
+		case 4:
+			System.out.println("更新します");
+			break;
+		}
+	}
+}
