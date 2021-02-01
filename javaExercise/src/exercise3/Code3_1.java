ecf9225d89d729f0616246ed9d5948ff43c55a80
ys194 <ys194@LAPTOP-M89T4L4C.mshome.net>
2021-02-01 17:18:07 +0900

    [Add] Code3_1.java

---
+++ javaExercise/src/exercise3/Code3_1.java
@@ -0,0 +1,13 @@
+package exercise3;
+
+public class Code3_1 {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯します");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+		}
+	}
+}
