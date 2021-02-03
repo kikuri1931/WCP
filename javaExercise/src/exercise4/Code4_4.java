855b09a2d70a64ecb50a84189fe13422aec7b0a6
ys194 <ys194@LAPTOP-M89T4L4C.mshome.net>
2021-02-03 19:46:31 +0900

    [Add] Code4_4.java

---
+++ javaExercise/src/exercise4/Code4_4.java
@@ -0,0 +1,11 @@
+package exercise4;
+
+public class Code4_4 {
+	public static void main(String[] args) {
+		int[] scores;
+		scores = new int[5];
+		scores[1] = 30;
+
+		System.out.println(scores[1]);
+	}
+}
