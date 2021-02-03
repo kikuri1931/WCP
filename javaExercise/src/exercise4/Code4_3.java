3fef18f3558ea0419a20aba74083f93a2efafef7
ys194 <ys194@LAPTOP-M89T4L4C.mshome.net>
2021-02-03 19:40:57 +0900

    [Add] Code4_3.java

---
+++ javaExercise/src/exercise4/Code4_3.java
@@ -0,0 +1,10 @@
+package exercise4;
+
+public class Code4_3 {
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		int num = scores.length;
+
+		System.out.println("要素の数：" + num);
+	}
+}
