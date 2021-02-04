69dde25188e4431e8a8cd3a0a29687cd24fa9e29
ys194 <ys194@LAPTOP-M89T4L4C.mshome.net>
2021-02-04 16:21:39 +0900

    [Add] Code4_12.java

---
+++ javaExercise/src/exercise4/Code4_12.java
@@ -0,0 +1,13 @@
+package exercise4;
+
+public class Code4_12 {
+	public static void main(String[] args) {
+		int[] arrayA = {1, 2, 3};
+		int[] arrayB;
+
+		arrayB = arrayA;
+		arrayB[0] = 100;
+
+		System.out.println(arrayB[0]);
+	}
+}
