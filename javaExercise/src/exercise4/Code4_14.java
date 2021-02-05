a541a67ab5fb2387edbaf98f440b2bcfc129a90a
ys194 <ys194@LAPTOP-M89T4L4C.mshome.net>
2021-02-05 19:49:52 +0900

    [Add] Code4_14.java

---
+++ javaExercise/src/exercise4/Code4_14.java
@@ -0,0 +1,10 @@
+package exercise4;
+
+public class Code4_14 {
+	public static void main(String[] args) {
+		int[][] scores = {{40, 50, 60}, {80, 70, 60}};
+
+		System.out.println(scores.length);
+		System.out.println(scores[0].length);
+	}
+}
